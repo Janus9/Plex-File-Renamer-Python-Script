21b115d7d194f96e6395775f0838ffad40e34c9d
Joshua Bayt <31780330+Janus9@users.noreply.github.com>
2025-07-09 13:35:51 -0700

    Add files via upload

--- plex_renamer.py
+++ plex_renamer.py
@@ -1,3 +1,21 @@
+"""
+plex_renamer.py
+author/ Janus Wheatley
+brief/ Bulk Rename Script for use in renaming TV Show season folders
+about/ Plex requires the following naming for TV Shows
+    [TV SHOW NAME]
+        [SEASON X]
+            [TV SHOW NAME + SEASON X + EPX]
+            ...
+how to/
+    ARGS -> python plex_renamer.py "[FOLDER DIRECTORY]" "[TV SHOW NAME] [SEASON]"
+    All files in the Folder Directory will be renamed to ARG 2 + their episode # which is counted
+    The files MUST be in order naturally or they will be renamed w/ the wrong #
+ex/
+    python plex_renamer.py "E:\TV Shows\Clone Wars\Clone Wars S01" "Clone Wars S01"
+
+This program is open source, feel free to edit/modify
+"""
 import sys
 import os
 
